f78c8da9337be4af38acdaef2194f1914825587a
Yousra Zahra LATER <yousra.later@univ-constantine2.dz>
2024-05-12 10:09:24 +0100

    'Last Commit'

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,60 @@
-import { Route,Routes } from 'react-router-dom'
-import { Login } from './pages/Login'
-function App() {
-  return (
-    <main className=' w-full min-h-screen bg-[#f3f4f6]'>
-      <Routes>
-        <Route element={<Layout/>}>
-          <Route path='/' element={<Navigate to='/dashboard'/>} />
-          <Route path='/dashboard' element={<Dashboard/>} />
-          <Route path='/tasks' element={<Tasks/>} />
-          <Route path='/completed/:status >' element={<Tasks/>} />
-          <Route path='/in-progress/:status >' element={<Tasks/>} />
-          <Route path='/todo/:status >' element={<Tasks/>} />
-          <Route path='/team >' element={<Users/>} />
-          <Route path='/trashed >' element={<Trash/>} />
-          <Route path='/task/:id >' element={<TaskDetails/>} />
-        </Route>
-      </Routes>
-    </main>
-  )
+import { Route, Routes, useLocation, Navigate, Outlet } from "react-router-dom";
+import { Toaster } from "sonner";
+import { Login } from "./pages/Login";
+import { Dashboard } from "./pages/Dashboard";
+import { Tasks } from "./pages/Tasks";
+import { Trash } from "./pages/Trash";
+import { TaskDetails } from "./pages/TaskDetails";
+import { Users } from "./pages/Users";
+
+function Layout() {
+  const user = ""; // Vous pouvez initialiser cela à un objet utilisateur s'il est connecté
+
+  const location = useLocation(); // Obtenez l'objet location actuel
+
+  return user ? ( // Si l'utilisateur est connecté
+    <div className="w-full h-screen flex flex-col md:flex-row">
+      <div className="w-1/5 h-screen bg-white sticky top-0 hidden md:block">
+        {/* SideBar*/}
+      </div>
+      {/* MobileBar */}
+      <div className="flex-1 overflow-auto">
+        {/* NavBar */}
+        <div className="p-4 2xl:px-10">
+          {/*Il agit comme un point de rendu dynamique dans votre composant parent (layout) où 
+          les composants enfants rendus par les routes sont affichés. */}
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  ) : (
+    // Si l'utilisateur n'est pas connecté 
+    <Navigate to="/log-in" state={{ from: location }} replace />
+  );
 }
 
-export default App
+function App() {
+  return (
+    <main className=" w-full min-h-screen bg-[#f3f4f6]">
+      <Routes>
+        <Route element={<Layout/>}>
+          {/*définit la mise en page de lapplication/ route parente/ agit comme un conteneur fixe pour toute lapplication*/}
+          {/*les routes de ses enfants qui changent */}
+          <Route path="/" element={<Navigate to="/dashboard" />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/tasks" element={<Tasks />} />
+          <Route path="/completed/:status >" element={<Tasks />} />
+          <Route path="/in-progress/:status >" element={<Tasks />} />
+          <Route path="/todo/:status >" element={<Tasks />} />
+          <Route path="/team >" element={<Users />} />
+          <Route path="/trashed >" element={<Trash />} />
+          <Route path="/task/:id >" element={<TaskDetails />} />
+        </Route>
+        <Route path="/log-in" element={<Login />} />
+      </Routes>
+      <Toaster richColors />
+    </main>
+  );
+}
+
+export default App;
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,11 +1,67 @@
-import React from 'react'
-import { Button } from '@headlessui/react'
+import React, { useEffect } from "react";
+import { useForm } from "react-hook-form";
+
+import { useNavigate } from "react-router-dom";
 export const Login = () => {
+  const user = "";
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const navigate = useNavigate();
+
+  const submitHabndler=async (data)=>{
+    console.log('submit');
+  }
+  //Cet effet est utile pour rediriger automatiquement l'utilisateur
+  //vers l'URL /dashboard dès qu'il se connecte à votre application.
+  useEffect(() => {
+    // Vérifie si 'user' est vrai (utilisateur connecté)
+    if (user) {
+      // Navigue vers l'URL '/dashboard'
+      navigate("/dashboard");
+    }
+  }, [user]); // Exécuter cet effet chaque fois que la valeur de 'user' change
+
   return (
-    <div>Login
-    <Button className="rounded bg-sky-600 ">
-    Save changes
-  </Button>
-  </div>
-  )
-}
+    <div className="w-full min-h-screen flex items-center justify-center flex-col lg:flex-row bg-white">
+      <div className="w-full md:w-auto flex gap-0 md:gap-40 flex-col md:flex-row items-center justify-center">
+        {/*left side*/}
+        <div className="h-full w-full lg:w-2/3 flex flex-col items-center justify-center">
+          <div className="w-full md:max-w-lg 2xl:max-w-3xl flex flex-col items-center justify-center gap-5 md:gap-y-10 2xl:-mt-20">
+            <span className="flex gap-1 py-1 px-3 border rounded-full text-sm md:text-base bordergray-300 text-gray-700">
+              Manage all your task in one place!
+            </span>
+            <p className="flex flex-col gap-0 md:gap-4 text-4xl md:text-6xl 2xl:text-7xl font-black text-center text-sky-600">
+              <span>Cloud-Based</span>
+              <span>Task Manager</span>
+            </p>
+              <div className="circle rotate-in-up-left"></div>
+          </div>
+        </div>
+        {/*right side*/}
+        <div className="w-full md:w-1/3 p-4 md:p-1 flex flex-col justify-center items-center">
+        <form onSubmit={handleSubmit(submitHabndler)} className='form-container w-full md:w-[400px] flex flex-col  gap-y-8 bg-white px-10 pt-14 pb-14'>
+          <div className="">
+           <p className="text-sky-600 text-3xl font-bold text-center">Welcome back!</p>
+           <p className="text-center text-base text-gray-700 mt-2"> Keep all your credential safe</p>
+          </div>
+           <div className="flex flex-col gap-y-5">
+            <Textbox 
+              placeholder="email@example.com"
+              type='email'
+              name='email'
+              label='Email Address'
+              className='w-full rounded-full'
+              register={register ("email", {required : 'Email Adress is required'})}
+              error={errors.email ? errors.email.message:''}
+            
+            />
+           </div>
+        </form>
+        </div>
+      </div>
+    </div>
+  );
+};
